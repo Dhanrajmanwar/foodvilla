d6d1466df09d5f8eb8c18df5c71a2dc5f874161b
Dhanraj Manwar <96405562+Dhanrajmanwar@users.noreply.github.com>
2024-07-09 15:55:38 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/foodvilla')
+    await mongoose.connect('mongodb+srv://dhanrajmanwar61:Dhanraj13@cluster0.w7bftvw.mongodb.net/foodvilla')
     .then(()=>console.log("DB connected"))
 }
